b495e94087ec11ce345bdd4959d701a338b1be24
phellipesnts <phellipe.santos@ufpe.br>
2025-07-22 14:51:05 -0300

    Create 2º exercício, parte 1 - aula 2 (C).c

---
+++ 2º exercício, parte 1 - aula 2 (C).c
@@ -0,0 +1,10 @@
+int main() {
+    printf("2º exercício, parte 1 - aula 2\n");
+
+    double raio = 5.0;
+    double area = PI * raio * raio;
+
+    printf("A área do círculo com raio %.2f é: %.2f\n", raio, area);
+
+    return 0;
+}
