f779b32a6909427daf4c22de5d08406812713b59
phellipesnts <phellipe.santos@ufpe.br>
2025-07-22 14:43:37 -0300

    Create 1º exercício, parte 1 - aula 2.py

---
+++ 1º exercício, parte 1 - aula 2.py
@@ -0,0 +1,8 @@
+pi = 3.14159
+
+print("1º exercício, parte 1 - aula 2\n")
+
+raio = float(input("digite o valor do raio da circunferência: "))
+area = pi * raio ** 2
+
+print("a área da circunferência é:", area)
