33c82d6b61ab6165b36e81fdebbf840dd6a4bc30
phellipesnts <phellipe.santos@ufpe.br>
2025-07-22 15:01:26 -0300

    Create 4º exercício, parte 1 - aula 2.py

---
+++ 4º exercício, parte 1 - aula 2.py
@@ -0,0 +1,5 @@
+print("4º exercício, parte 1 - aula 2.py\n")
+
+celsius = float(input("digite a temperatura em celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print("temperatura em Fahrenheit:", fahrenheit)
