017995dd75277915e3c5a6ec227e0c44d8a3129b
phellipesnts <phellipe.santos@ufpe.br>
2025-07-22 15:04:26 -0300

    Create 5º exercício, parte 1 - aula 2.py

---
+++ 5º exercício, parte 1 - aula 2.py
@@ -0,0 +1,8 @@
+print("5º exercício, parte 1 - aula 2\n")
+
+x = float(input("digite x: "))
+y = float(input("digite y: "))
+z = float(input("digite z: "))
+
+modulo = (x**2 + y**2 + z**2) ** 0.5
+print("módulo do vetor:", modulo)
